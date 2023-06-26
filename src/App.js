c1070607e4e80413a8169d2a6fe59e08135162e4
Philippe Miossec <pmiossec@gmail.com>
2023-06-26 16:53:49 +0200

    Some react initial state

--- src/App.js
+++ src/App.js
@@ -1,9 +1,18 @@
-import logo from './logo.svg';
 import './App.css';
+import Navbar from './components/Navbar';
+import { Route, Routes } from 'react-router-dom';
+import HomePage from './pages/HomePage';
 
 function App() {
   return (
     <div className="App">
+      {/* Below: ADD <Navbar>, <Routes> & <Route> */}
+
+      <Navbar />
+
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+      </Routes>
     </div>
   );
 }
